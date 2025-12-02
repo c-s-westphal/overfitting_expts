23b575c392f4bba17f850ce5f26980b0ed234682
c-s-westphal <c.s.westphal@outlook.com>
2025-12-02 12:48:34 +0000

    Revert "fix: train for fixed epochs instead of early stopping in exp9"
    This reverts commit fa4c0e54492d2694f922a12b91628ae283343ea7.

--- scripts/job_manager_exp9.sh
+++ scripts/job_manager_exp9.sh
@@ -71,8 +71,8 @@ python3.9 -u experiments/exp9_single_run.py \
     --batch_size 128 \
     --device cuda \
     --output_dir results/exp9 \
-    --epochs 100 \
-    --retrain_epochs 5 \
+    --max_epochs 200 \
+    --retrain_epochs 10 \
     --lr 0.001
 
 date
